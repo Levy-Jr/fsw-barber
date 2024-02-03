6b0f56250a5b43f16bd44285f8bc5eb5475b07cb
Levy-Jr <levybusinessjr@gmail.com>
2024-02-03 09:40:29 -0400

    feat: ensure confirmed bookings are shown on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,14 +2,31 @@ import { format } from 'date-fns'
 import Header from "../_components/Header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_componentes/Search";
-import BookingItem from "../_components/Booking-item";
 import { db } from '../_lib/prisma';
 import BarbershopItem from './_componentes/Barbershop-Item';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+import BookingItem from '../_components/Booking-item';
 
 export default async function Home() {
-  const barbershops = await db.barbershop.findMany({
+  const session = await getServerSession(authOptions)
 
-  })
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user ? db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          gte: new Date()
+        }
+      },
+      include: {
+        service: true,
+        barbershop: true
+      }
+    })
+      : Promise.resolve([])
+  ])
 
   return (
     <div className="">
@@ -28,10 +45,16 @@ export default async function Home() {
         <Search />
       </div>
 
-      {/*       <div className="px-5 mt-6">
-        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-        <BookingItem />
-      </div> */}
+      <div className="mt-6">
+        <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+
+        <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map(booking => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
+
+      </div>
 
       <div className="mt-6 mb-[4.5rem]">
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
